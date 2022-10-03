224906b276c65f6fc0e5c26db099ee0a7e6a8a72
digiDreams <digital2dreamer@gmail.com>
2022-10-03 11:45:10 +0200

    First commit: creating repository

---
+++ cineApp/cineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  cineAppApp.swift
+//  cineApp
+//
+//  Created by Apprenant 16 on 03/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct cineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
